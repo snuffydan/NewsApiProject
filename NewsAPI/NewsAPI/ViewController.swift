c5066a74a647d03322a86dc9120d413694fe548a
danushika.priyadarshani <danushikadan@gmail.com>
2022-06-02 22:45:49 +0530

    Project creation

---
+++ NewsAPI/NewsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAPI
+//
+//  Created by Danushika Priyadarshani on 2022-06-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
