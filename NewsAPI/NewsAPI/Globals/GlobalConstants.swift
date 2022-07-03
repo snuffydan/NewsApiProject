784038510d9108872d15c6a891cd732545d79db3
Danushika Priyadarshani <danushikadan@gmail.com>
2022-07-03 11:33:03 +0530

    Added login and register views

---
+++ NewsAPI/NewsAPI/Globals/GlobalConstants.swift
@@ -0,0 +1,16 @@
+//
+//  GlobalConstants.swift
+//  NewsAPI
+//
+//  Created by Danushika Priyadarshani on 2022-06-10.
+//
+
+import UIKit
+
+let PASSWORD_LENGTH = 6
+
+
+let EMAIL_VALIDATION = "Please enter a valid email"
+let USERNAME_VALIDATION = "Please enter a username"
+let PASSWORD_VALIDATION = "Password should be more than \(PASSWORD_LENGTH) characters"
+let CONFIRM_PASSWORD_VALIDATION = "Pssswords do not match"
---
+++ NewsAPI/NewsAPI/Globals/GlobalEnums.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalEnums.swift
+//  NewsAPI
+//
+//  Created by Danushika Priyadarshani on 2022-06-10.
+//
+
+import UIKit
+
+enum StoryboardIdentifiers: String {
+    case UserInfo = "UserInfo"
+}
+
+enum ViewControllerIdentifiers: String {
+    case LoginVC = "loginVC"
+    case RegisterVC = "registerVC"
+}
